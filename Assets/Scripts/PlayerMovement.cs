2b6054dd7e7723228997f2e6d05aecee89a3704b
Jack Johnson <jrj221@byu.edu>
2026-04-11 10:19:53 -0600

    Setting velocity using Vector3.SmoothDamp to make it feel smoother and less start-stop
    This interferes with unity physics, but this project doesn't interact with physics much so it's okay

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speed;
     private Rigidbody _rb;
     private Vector3 _moveDirection;
+    private Vector3 _velocitySmoothing = Vector3.zero;
 
     private void Awake()
     {
@@ -21,8 +22,13 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 inputMoveDirection = InputManager.Instance.InputMoveDirection;
         _moveDirection = Vector3.ProjectOnPlane(inputMoveDirection.x * transform.right + inputMoveDirection.y * transform.forward, Vector3.up).normalized;
-        _rb.AddForce(10f * _speed * _moveDirection);
+        // _rb.AddForce(10f * _speed * _moveDirection);
+        _rb.linearVelocity = Vector3.SmoothDamp(_rb.linearVelocity, 
+                                                _moveDirection * _speed,  
+                                                ref _velocitySmoothing, 
+                                                0.05f);
     }
+    
 
     private void CapSpeed()
     {
